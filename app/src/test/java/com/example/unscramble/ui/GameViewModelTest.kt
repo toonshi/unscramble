3edb9b7930681570073d221da4cdc5cfc5992b15
Mike Agoya <agoyamike@gmail.com>
2026-01-28 12:14:34 +0300

    Add test case for Error path

--- app/src/test/java/com/example/unscramble/ui/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
@@ -32,4 +33,19 @@ class GameViewModelTest {
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+        //for an incorrect word as input
+        val incorrectPlayerWord = "lim"
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+        val currentGameUiState = viewModel.uiState.value
+
+        //check if score is 0
+        assertEquals(0, currentGameUiState.score)
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+
+    }
+
+
 }
